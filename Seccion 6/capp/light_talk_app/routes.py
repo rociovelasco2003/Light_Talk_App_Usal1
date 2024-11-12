e37710dd61ae4c18dcef2fbd1ce4db8cd2f3b469
rociovelasco2003 <rociovelasco@usal.es>
2024-11-12 18:04:59 +0100

    Delete Seccion 6 directory

--- Seccion 6/capp/light_talk_app/routes.py
+++
@@ -1,8 +0,0 @@
-from flask import render_template, Blueprint
-
-light_talk_app=Blueprint('light_talk_app',__name__)
-
-@light_talk_app.route('/light_talk_app')
-def light_talk_app_home():
-    return render_template('light_talk_app.html', title='light_talk_app')
-    
